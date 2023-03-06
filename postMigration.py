95ed406beae8f113704d829cee188f98e96f5570
sheriffoladejo <sherifffoladejo@gmail.com>
2023-03-06 10:50:54 +0100

    Default commit

--- postMigration.py
+++ postMigration.py
@@ -59,8 +59,7 @@ def getPostData(id):
     database = connect1_0()
     cursor = database.cursor()
     cursor.execute("select * from engine4_activity_actions where action_id > " + str(
-        id) + " and Date(date) > '2022-01-01 00:00:00' and (type = 'post' or type = 'cover_photo_update' or "
-              "type = 'profile_photo_update' or type = 'post_self_photo' or type = 'post_self') limit 5000")
+        id) + " and Date(date) > '2022-01-01 00:00:00' and (type = 'post' or type = 'post_self_photo' or type = 'post_self') limit 5000")
     result = cursor.fetchall()
 
     last_id = 0
